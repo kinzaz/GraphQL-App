aabecf46233981b1d871a8a0497343131578c8cd
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-04-29 17:57:52 +0300

    back graphql

---
+++ server/mock.js
@@ -0,0 +1,10 @@
+const users = [
+	{ id: 1, username: 'Vlad', age: 30 },
+	{ id: 2, username: 'Dima', age: 31 },
+	{ id: 3, username: 'Araz', age: 37 },
+	{ id: 4, username: 'Arina', age: 35 },
+	{ id: 5, username: 'Edik', age: 32 },
+	{ id: 6, username: 'Others', age: 33 },
+];
+
+module.exports = users;
